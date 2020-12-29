27aa31f7beccdbf274cbe63bbb10485ac9933647
Simone <simone.fiorentini@sonovate.com>
2020-12-29 23:57:51 +0100

    added django-heroku

--- carpediem/carpediem/settings.py
+++ carpediem/carpediem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+django_heroku.settings(locals())
